d104ff30448f57448315167a04e2b8038834f5f5
bjeutter <jeutter@zeichenkraftwerk.de>
2015-08-18 22:25:25 +0200

    Refactoring

--- assistance-platform-service/app/Global.java
+++ assistance-platform-service/app/Global.java
@@ -7,23 +7,24 @@ import play.GlobalSettings;
 import play.Logger;
 import play.libs.Akka;
 
-
 public class Global extends GlobalSettings {
-	  @Override
-	  public void onStart(Application app) {
-	    Logger.info("Application has started");
-	    
-	    ModuleAliveChecker aliveChecker = new ModuleAliveChecker();
-	    
-	    Akka.system().scheduler().schedule(Duration.create(0, TimeUnit.MILLISECONDS), //Initial delay 0 milliseconds
-	    	    Duration.create(30, TimeUnit.MINUTES),     //Frequency 30 minutes
-	    	    new Runnable() {
-                    @Override
-                    public void run() {
-                        aliveChecker.checkModulesAliveStati();
-                    }
-                },
-                Akka.system().dispatcher()
-	    	);
-	    }  
+	@Override
+	public void onStart(Application app) {
+		schedulePeriodicModuleCheck();
+	}
+
+	private void schedulePeriodicModuleCheck() {
+		    ModuleAliveChecker aliveChecker = new ModuleAliveChecker();
+		    
+		    Akka.system().scheduler().schedule(Duration.create(0, TimeUnit.MILLISECONDS), //Initial delay 0 milliseconds
+		    	    Duration.create(30, TimeUnit.MINUTES),     //Frequency 30 minutes
+		    	    new Runnable() {
+	                    @Override
+	                    public void run() {
+	                        aliveChecker.checkModulesAliveStati();
+	                    }
+	                },
+	                Akka.system().dispatcher()
+		    	);F
+	  }
 }
