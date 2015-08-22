96591a962408b7e812d54e8e79a61d0d9a3692de
bjeutter <jeutter@zeichenkraftwerk.de>
2015-08-22 20:59:30 +0200

    Removed 9th db evolution

--- assistance-platform-service/conf/evolutions/default/2.sql
+++ assistance-platform-service/conf/evolutions/default/2.sql
@@ -3,14 +3,15 @@
 # --- !Ups
  
 CREATE TABLE active_modules (
-    id varchar(255) PRIMARY KEY,
+    id varchar(255) NOT NULL,
     name varchar(60) NOT NULL,
     logo_url varchar(2048) NOT NULL,
     description_short varchar(255) NOT NULL,
     description_long varchar(2048) NOT NULL,
     required_capabilities varchar(1024) NOT NULL,
     optional_capabilities varchar(1024) NOT NULL,
-    copyright varchar(255) NOT NULL
+    copyright varchar(255) NOT NULL,
+    ALTER TABLE active_module_localization ADD PRIMARY KEY (module_id, language_code)
 );
  
 # --- !Downs
