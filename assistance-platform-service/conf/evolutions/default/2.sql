9cf61a4dfd8d1ed2313d2a777885c14142f68df8
bjeutter <jeutter@zeichenkraftwerk.de>
2015-08-22 21:02:42 +0200

    DB Fix

--- assistance-platform-service/conf/evolutions/default/2.sql
+++ assistance-platform-service/conf/evolutions/default/2.sql
@@ -3,15 +3,14 @@
 # --- !Ups
  
 CREATE TABLE active_modules (
-    id varchar(255) NOT NULL,
+    id varchar(255) PRIMARY KEY,
     name varchar(60) NOT NULL,
     logo_url varchar(2048) NOT NULL,
     description_short varchar(255) NOT NULL,
     description_long varchar(2048) NOT NULL,
     required_capabilities varchar(1024) NOT NULL,
     optional_capabilities varchar(1024) NOT NULL,
-    copyright varchar(255) NOT NULL,
-    PRIMARY KEY (module_id, language_code)
+    copyright varchar(255) NOT NULL
 );
  
 # --- !Downs
--- assistance-platform-service/conf/evolutions/default/5.sql
+++ assistance-platform-service/conf/evolutions/default/5.sql
@@ -3,12 +3,13 @@
 # --- !Ups
  
 CREATE TABLE active_module_localization (
-    module_id varchar(255) PRIMARY KEY,
+    module_id varchar(255) NOT NULL,
     language_code varchar(2) NOT NULL,
 	name varchar(60) NOT NULL,
     logo_url varchar(2048) NOT NULL,
     description_short varchar(255) NOT NULL,
-    description_long varchar(2048) NOT NULL
+    description_long varchar(2048) NOT NULL,
+    PRIMARY KEY (module_id, language_code)
 );
  
 # --- !Downs
