839a0b965851d009994a246023101e62184336d2
bjeutter <jeutter@zeichenkraftwerk.de>
2015-08-26 10:33:51 +0200

    serialUID for bundle info missing exception

--- AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/modules/exceptions/ModuleBundleInformationMissingException.java
+++ AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/modules/exceptions/ModuleBundleInformationMissingException.java
@@ -4,6 +4,8 @@ package de.tudarmstadt.informatik.tk.assistanceplatform.modules.exceptions;
  * This exception is thrown when bundle informations are not provided / implemented.
  */
 public class ModuleBundleInformationMissingException extends Exception {
+	private static final long serialVersionUID = -5287998614962883514L;
+
 	public ModuleBundleInformationMissingException(String s) {
 		super(s);
 	}
