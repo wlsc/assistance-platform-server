4788e7ab8ad317e718e4f69f54cd3f7a0e91d048
bjeutter <jeutter@zeichenkraftwerk.de>
2015-09-06 23:33:54 +0200

    Refined ClientActionRunner interface

--- AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/services/action/DummyClientActionRunner.java
+++ AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/services/action/DummyClientActionRunner.java
@@ -1,14 +1,10 @@
 package de.tudarmstadt.informatik.tk.assistanceplatform.services.action;
 
+import java.util.Arrays;
+
 import org.apache.log4j.Logger;
 
 public class DummyClientActionRunner implements IClientActionRunner {
-
-	@Override
-	public void showMessage(long userId, long deviceId, String message) {
-		Logger.getLogger(this.getClass()).info( String.format("CLIENT ACTION: Send message to device %d of user %d", deviceId, userId) );
-		
-	}
 
 	@Override
 	public void displayInformation() {
@@ -18,8 +14,17 @@ public class DummyClientActionRunner implements IClientActionRunner {
 
 	@Override
 	public void sendMail() {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 
+	@Override
+	public void showMessage(long userId, long[] deviceIds, String title,
+			String message) {
+		Logger.getLogger(this.getClass()).info( String.format("CLIENT ACTION: Send message to devices %s of user %d", Arrays.toString(deviceIds), userId) );
+	}
+
+	@Override
+	public void sendTestData(long userId, long[] deviceIds, String data) {
+		Logger.getLogger(this.getClass()).info( String.format("CLIENT ACTION: Send data to devices %s of user %d", Arrays.toString(deviceIds), userId) );
+	}
 }
--- AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/services/action/IClientActionRunner.java
+++ AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/services/action/IClientActionRunner.java
@@ -1,7 +1,15 @@
 package de.tudarmstadt.informatik.tk.assistanceplatform.services.action;
 
 public interface IClientActionRunner {
-	void showMessage(long userId, long deviceId, String message);
+	void showMessage(long userId, long deviceIds[], String title, String message);
+	/**
+	 * FOR TESTING ONLY!!! NOT FOR PRODUCTION MODE!
+	 * @param userId
+	 * @param deviceId
+	 * @param title
+	 * @param message
+	 */
+	void sendTestData(long userId, long deviceIds[], String data);
 	void displayInformation();
 	void sendMail();
 }
