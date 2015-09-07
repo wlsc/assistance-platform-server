6107dd6cef8928f0e32df06f3cb69334a86a3561
bjeutter <jeutter@zeichenkraftwerk.de>
2015-09-08 00:05:16 +0200

    Increased model name colum length for devices

---
+++ assistance-platform-service/conf/evolutions/default/12.sql
@@ -0,0 +1,9 @@
+# Increase size of model column of devices
+ 
+# --- !Ups
+
+ALTER TABLE devices ALTER COLUMN model TYPE varchar(100);
+ 
+# --- !Downs
+ 
+ALTER TABLE devices ALTER COLUMN model TYPE varchar(30);
