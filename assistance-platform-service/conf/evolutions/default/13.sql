0ae64d4d8a87381d6b41f8b7bdb4b16cbd12077a
bjeutter <jeutter@zeichenkraftwerk.de>
2015-09-08 14:02:05 +0200

    Added new db evolution to add user_defined_name and last_usage for devices

---
+++ assistance-platform-service/conf/evolutions/default/13.sql
@@ -0,0 +1,11 @@
+# Add user defined name and last_usage timestamp for devices
+ 
+# --- !Ups
+ 
+ALTER TABLE devices ADD COLUMN user_defined_name VARCHAR(50);
+ALTER TABLE devices ADD COLUMN last_usage TIMESTAMP;
+ 
+# --- !Downs
+ 
+ALTER TABLE devices DROP user_defined_name;
+ALTER TABLE devices DROP last_usage;
