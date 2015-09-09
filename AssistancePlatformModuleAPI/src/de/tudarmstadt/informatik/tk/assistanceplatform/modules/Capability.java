8ada2181c6dfe8d01768e76d016487fbe70300df
bjeutter <jeutter@zeichenkraftwerk.de>
2015-09-09 21:26:10 +0200

    Changed Capability class to it reflects a more fine grained control over collection / update frequencies and the required number of sensor readings

--- AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/modules/Capability.java
+++ AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/modules/Capability.java
@@ -7,16 +7,30 @@ public class Capability {
 	public String type;
 	
 	/**
-	 * The frequency of needed event readings of this type (per second)
+	 * The frequency of needed event readings of this type (per second). Readings can be cached on the client.
 	 */
-	public double frequency;
+	public double collection_frequency;
+	
+	/**
+	 * The frequency in which at least {@link #min_required_readings_on_update} have to be sent to the platform.
+	 */
+	public double required_update_frequency;
+	
+	/**
+	 * The minimum number that has to be sent in {@link #required_update_frequency} so the module can keep up with processing.
+	 */
+	public int min_required_readings_on_update;
 
 	public Capability() {
 	}
-	
-	public Capability(String type, double frequency) {
+
+	public Capability(String type, double collection_frequency,
+			double required_update_frequency,
+			int min_required_readings_on_update) {
 		super();
 		this.type = type;
-		this.frequency = frequency;
+		this.collection_frequency = collection_frequency;
+		this.required_update_frequency = required_update_frequency;
+		this.min_required_readings_on_update = min_required_readings_on_update;
 	}
 }
