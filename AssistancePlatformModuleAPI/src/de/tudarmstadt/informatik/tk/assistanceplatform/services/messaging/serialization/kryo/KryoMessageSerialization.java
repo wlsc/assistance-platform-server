c5a26945d9be76bef9a0fe1d7fad8f373d33bc5a
bjeutter <jeutter@zeichenkraftwerk.de>
2015-10-05 22:32:20 +0200

    Added serialization of UUIDs for Kryo

---
+++ AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/services/messaging/serialization/kryo/KryoMessageSerialization.java
@@ -0,0 +1,66 @@
+package de.tudarmstadt.informatik.tk.assistanceplatform.services.messaging.serialization.kryo;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.pool.KryoFactory;
+import com.esotericsoftware.kryo.pool.KryoPool;
+
+import de.tudarmstadt.informatik.tk.assistanceplatform.services.messaging.serialization.MessageSerialization;
+
+/**
+ * Implements rudimentary binary object serialization via Kryo Library
+ */
+public class KryoMessageSerialization implements MessageSerialization {
+	private final KryoPool pool;
+
+	public KryoMessageSerialization() {
+		KryoFactory factory = new KryoFactory() {
+			public Kryo create() {
+				Kryo kryo = new Kryo();
+				kryo.addDefaultSerializer(UUID.class, UUIDSerializer.class);
+				// configure kryo instance, customize settings
+				return kryo;
+			}
+		};
+		// Build pool with SoftReferences enabled (optional)
+		pool = new KryoPool.Builder(factory).softReferences().build();
+	}
+
+	@Override
+	public <T> byte[] serialize(T data) {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		Output output = new Output(outputStream);
+
+		Kryo kryo = pool.borrow();
+
+		kryo.writeObject(output, data);
+
+		pool.release(kryo);
+
+		byte[] result = output.getBuffer();
+
+		output.close();
+
+		return result;
+	}
+
+	@Override
+	public <T> T deserialize(byte[] data, Class<T> type) {
+		Input input = new Input(data);
+
+		Kryo kryo = pool.borrow();
+
+		T obj = kryo.readObject(input, type);
+
+		pool.release(kryo);
+
+		input.close();
+
+		return obj;
+	}
+
+}
---
+++ AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/services/messaging/serialization/kryo/UUIDSerializer.java
@@ -0,0 +1,29 @@
+package de.tudarmstadt.informatik.tk.assistanceplatform.services.messaging.serialization.kryo;
+
+import java.util.UUID;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+public class UUIDSerializer extends Serializer<UUID> {
+
+	@Override
+	public UUID read(Kryo arg0, Input arg1, Class<UUID> arg2) {
+		long least = arg1.readLong();
+		long most = arg1.readLong();
+		
+		return new UUID(most, least);
+	}
+
+	@Override
+	public void write(Kryo arg0, Output arg1, UUID arg2) {
+		long least = arg2.getLeastSignificantBits();
+		long most = arg2.getMostSignificantBits();
+		
+		arg1.writeLong(least);
+		arg1.writeLong(most);
+	}
+
+}
