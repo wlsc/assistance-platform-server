99dd5a09c652e672f709f5766f416a629ad3db9c
bjeutter <jeutter@zeichenkraftwerk.de>
2015-10-12 14:20:02 +0200

    Removed old code

--- assistance-platform-service/app/controllers/Application.java
+++ assistance-platform-service/app/controllers/Application.java
@@ -9,8 +9,4 @@ public class Application extends Controller {
        // return ok(index.render("Your new application is ready."));
     	return ok("Your new application is ready.");
     }
-    
-    public Result swagger() {
-    	return ok(views.html.swagger.render());
-    }
 }
