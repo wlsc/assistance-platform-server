70ee209f5da422b7cb86b5664011a480e695e19f
bjeutter <jeutter@zeichenkraftwerk.de>
2015-10-15 21:22:46 +0200

    Removed usage of async cassandra stuff since it seems to not provide any gains currently

--- AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/persistency/cassandra/CassandraSensorDataPersistency.java
+++ AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/persistency/cassandra/CassandraSensorDataPersistency.java
@@ -32,7 +32,7 @@ public class CassandraSensorDataPersistency implements ISensorDataPersistency {
 	public boolean pesist(SensorData data) {
 		Statement s = createSaveStatement(data);
 		
-		cassandraSession.executeAsync(s); 
+		cassandraSession.execute(s); 
 		
 		return true;
 	}
