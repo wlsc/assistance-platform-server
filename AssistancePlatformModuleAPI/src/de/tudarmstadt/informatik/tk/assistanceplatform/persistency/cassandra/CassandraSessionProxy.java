b37f82ea96b40e7ed3026f99e68a2b1c45485d31
bjeutter <jeutter@zeichenkraftwerk.de>
2015-10-29 14:03:25 +0100

    Fixed docker conf for play framework

--- AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/persistency/cassandra/CassandraSessionProxy.java
+++ AssistancePlatformModuleAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/persistency/cassandra/CassandraSessionProxy.java
@@ -1,18 +1,36 @@
 package de.tudarmstadt.informatik.tk.assistanceplatform.persistency.cassandra;
 
+import java.net.InetAddress;
+import java.util.function.Consumer;
+
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Cluster.Builder;
 import com.datastax.driver.core.Session;
 
 public class CassandraSessionProxy {
 	private Cluster cluster;
 	private Session session;
 	
+	public CassandraSessionProxy(InetAddress[] contactPoints, String keystoreName) {
+		this((b) -> b.addContactPoints(contactPoints), keystoreName);
+	}
+	
 	public CassandraSessionProxy(String[] contactPoints, String keystoreName) {
-		cluster = Cluster.builder()
-				.addContactPoints(contactPoints)
-				.build();
+		this((b) -> b.addContactPoints(contactPoints), keystoreName); 
+	}
+	
+	private CassandraSessionProxy(Consumer<Builder> clusterBuilderSetter, String keystoreName) {
+		setCluster(clusterBuilderSetter);
 		
 		session = cluster.connect(keystoreName);
+	}
+	
+	private void setCluster(Consumer<Builder> clusterBuilderSetter) {
+		Builder b = Cluster.builder();
+		
+		clusterBuilderSetter.accept(b);
+		
+		cluster = b.build();
 	}
 	
 	public Session getSession() {
--- assistance-platform-service/app/persistency/ConfiguredSensorPersistencyProxy.java
+++ assistance-platform-service/app/persistency/ConfiguredSensorPersistencyProxy.java
@@ -1,32 +1,48 @@
 package persistency;
 
+import java.net.InetAddress;
+import java.util.LinkedList;
 import java.util.List;
 
+import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 import de.tudarmstadt.informatik.tk.assistanceplatform.persistency.ISensorDataPersistency;
 import de.tudarmstadt.informatik.tk.assistanceplatform.persistency.cassandra.CassandraSensorDataPersistency;
-import de.tudarmstadt.informatik.tk.assistanceplatform.persistency.cassandra.CassandraSessionProxy;;
+import de.tudarmstadt.informatik.tk.assistanceplatform.persistency.cassandra.CassandraSessionProxy;
 
 public class ConfiguredSensorPersistencyProxy {
 	private ISensorDataPersistency sensorDataPersistency;
-	
+
 	public ConfiguredSensorPersistencyProxy() {
-		CassandraSessionProxy sessionProxy = new CassandraSessionProxy(getContactPoints(), getKeystoreName());
-		
+
+		CassandraSessionProxy sessionProxy = new CassandraSessionProxy(
+				getContactPoints(), getKeystoreName());
+
 		sensorDataPersistency = new CassandraSensorDataPersistency(sessionProxy);
 	}
-	
-	private String[] getContactPoints() {
-		List<String> contactPoints = ConfigFactory.defaultApplication().getStringList("cassandra.contactPoints");
-		
-		return contactPoints.toArray(new String[contactPoints.size()]);
+
+	private InetAddress[] getContactPoints() {
+		List<String> contactPoints = null;
+
+		Config c = ConfigFactory.defaultApplication().resolve();
+
+		contactPoints = c.getStringList("cassandra.contactPoints");
+
+		return contactPoints.stream().map((s) -> {
+			try {
+				return InetAddress.getByName(s);
+			} catch (Exception e) {
+			}
+			return null;
+		}).toArray(size -> new InetAddress[size]);
 	}
-	
+
 	private String getKeystoreName() {
-		return ConfigFactory.defaultApplication().getString("cassandra.keystoreName");
+		return ConfigFactory.defaultApplication().getString(
+				"cassandra.keystoreName");
 	}
-	
+
 	public ISensorDataPersistency getSensorDataPersistency() {
 		return sensorDataPersistency;
 	}
