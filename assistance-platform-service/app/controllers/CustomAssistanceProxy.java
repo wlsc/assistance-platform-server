6265e144b21dbcdade9d8c3302444662f05d3c9d
bjeutter <jeutter@zeichenkraftwerk.de>
2015-11-05 18:59:05 +0100

    Added proxy endpoint which redirects custom requests direclty to the respective module rest server

---
+++ assistance-platform-service/app/controllers/CustomAssistanceProxy.java
@@ -0,0 +1,74 @@
+package controllers;
+
+import java.util.function.Function;
+
+import models.ActiveAssistanceModule;
+import persistency.UserModuleActivationPersistency;
+import play.libs.F.Promise;
+import play.libs.ws.WS;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
+import play.mvc.Result;
+import play.mvc.Results;
+import play.mvc.Security;
+import play.mvc.Http.RequestBody;
+import errors.AssistanceAPIErrors;
+
+/**
+ * This controller acts as a proxy for module rest services
+ * 
+ * @author bjeutter
+ */
+public class CustomAssistanceProxy extends RestController {
+	@Security.Authenticated(UserAuthenticator.class)
+	public Promise<Result> customGet(String moduleId, String path) {		
+		return custom(moduleId, path, (r) -> {
+			return r.get();
+		});
+	}
+	
+	@Security.Authenticated(UserAuthenticator.class)
+	public Promise<Result> customPost(String moduleId, String path) {
+		return custom(moduleId, path, (r) -> {
+			RequestBody body = request().body();
+			
+			String postBody = null;
+			
+			if(body.asText() != null) {
+				postBody = body.asText();
+			} else if(body.asJson() != null) {
+				r.setContentType("application/json");
+				postBody = body.asJson().toString();
+			} else if(body.asXml() != null) {
+				r.setContentType("application/xml");
+				postBody = body.asXml().toString();
+			}
+			
+			return r.post(postBody);
+		});
+	}
+
+
+	private Promise<Result> custom(
+			String moduleId,
+			String path,
+			Function<WSRequest, Promise<WSResponse>> prepareAndFireRequest) {
+		long userId = getUserIdForRequest();
+
+		if (!UserModuleActivationPersistency.doesActivationExist(userId,
+				moduleId)) {
+			return Promise
+					.pure(badRequestJson(AssistanceAPIErrors.moduleActivationNotActive));
+		}
+		
+		ActiveAssistanceModule module = UserModuleActivationPersistency.activatedModuleEndpointsForUser(new String[] { moduleId })[0];
+
+		WSRequest request = WS.url( module.restUrl("/custom/" + path) );
+		Promise<WSResponse> responsePromise = prepareAndFireRequest.apply(request);
+
+		return responsePromise.map((response) -> {
+			return (Result) Results.status(response.getStatus(),
+					response.getBody());
+		});
+	}
+}
