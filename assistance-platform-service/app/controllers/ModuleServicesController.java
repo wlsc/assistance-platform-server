ce57ea88a0f396b1dd61710096843be3d5fac828
bjeutter <jeutter@zeichenkraftwerk.de>
2015-11-07 19:15:50 +0100

    Started module service endpoint for database stuff

---
+++ assistance-platform-service/app/controllers/ModuleServicesController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.Result;
+
+public class ModuleServicesController extends RestController {
+	public Result database(String moduleId) {
+		// TODO: Generate a passowrd for the new user
+		// TODO: Create User in Cassandra
+		// TODO: Grante right permissions to the newly created user
+		// TODO: Return user / password pair
+		
+		return TODO;
+	}
+}
