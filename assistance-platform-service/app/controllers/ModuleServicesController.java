c6ab9fdb33f46ff19a22e5f32a2c2b31c96ffffd
bjeutter <jeutter@zeichenkraftwerk.de>
2015-11-16 14:37:43 +0100

    Added endpoint for Apache Spark Config

--- assistance-platform-service/app/controllers/ModuleServicesController.java
+++ assistance-platform-service/app/controllers/ModuleServicesController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import com.typesafe.config.ConfigFactory;
+
 import persistency.ActiveAssistanceModulePersistency;
 import persistency.cassandra.config.CassandraConfig;
 import persistency.cassandra.usercreation.CassandraModuleUser;
@@ -7,24 +9,38 @@ import persistency.cassandra.usercreation.CassandraModuleUserCreator;
 import play.libs.Json;
 import play.mvc.Result;
 import de.tudarmstadt.informatik.tk.assistanceplatform.services.internal.http.assistanceplatformservice.response.CassandraServiceConfigResponse;
+import de.tudarmstadt.informatik.tk.assistanceplatform.services.internal.http.assistanceplatformservice.response.ServiceConfigResponse;
 import errors.AssistanceAPIErrors;
 
 public class ModuleServicesController extends RestController {
 	private CassandraModuleUserCreator userCreator = new CassandraModuleUserCreator();
-	
+
 	public Result database(String moduleId) {
 		if (!ActiveAssistanceModulePersistency.doesModuleWithIdExist(moduleId)) {
 			return badRequestJson(AssistanceAPIErrors.moduleDoesNotExist);
 		}
 
 		CassandraModuleUser user = userCreator.createUserForModule(moduleId);
-		
+
 		CassandraServiceConfigResponse response = new CassandraServiceConfigResponse(
 				user.user, user.password,
-				CassandraConfig.getContactPointsArray(),
-				user.keyspace
-				);
+				CassandraConfig.getContactPointsArray(), user.keyspace);
 
 		return ok(Json.toJson(response));
+	}
+
+	public Result spark(String moduleId) {
+		if (!ActiveAssistanceModulePersistency.doesModuleWithIdExist(moduleId)) {
+			return badRequestJson(AssistanceAPIErrors.moduleDoesNotExist);
+		}
+
+		ServiceConfigResponse response = new ServiceConfigResponse(null, null,
+				new String[] { getSparkMaster() });
+
+		return ok(Json.toJson(response));
+	}
+
+	private String getSparkMaster() {
+		return ConfigFactory.defaultApplication().getString("spark.master");
 	}
 }
