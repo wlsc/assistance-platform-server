1ed0282757ae7af0613ab0a28c4a2c2e61cc0e24
bjeutter <jeutter@zeichenkraftwerk.de>
2015-11-17 21:46:33 +0100

    First docker-compose file + adustjment that web service waits for cassandra to be up

--- assistance-platform-service/app/persistency/cassandra/ConfiguredSensorPersistencyProxy.java
+++ assistance-platform-service/app/persistency/cassandra/ConfiguredSensorPersistencyProxy.java
@@ -1,5 +1,6 @@
 package persistency.cassandra;
 
+import play.Logger;
 import de.tudarmstadt.informatik.tk.assistanceplatform.persistency.IUserDeviceEventPersistency;
 import de.tudarmstadt.informatik.tk.assistanceplatform.persistency.cassandra.CassandraSensorDataPersistency;
 
@@ -7,6 +8,14 @@ public class ConfiguredSensorPersistencyProxy {
 	private IUserDeviceEventPersistency sensorDataPersistency;
 
 	public ConfiguredSensorPersistencyProxy() {
+		Logger.info("Start delay (30 sec) for cassandra ...");
+		try {
+			Thread.sleep(30 * 1000);
+		} catch (InterruptedException e) {
+			Logger.error("Thread sleep failed on waiting for cassandra", e);
+		}
+		Logger.info("Finished heavy delay for cassandra.");
+		
 		sensorDataPersistency = new CassandraSensorDataPersistency(CassandraSessionProxyFactory.getSessionProxy());
 	}
 
