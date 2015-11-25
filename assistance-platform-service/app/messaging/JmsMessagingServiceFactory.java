27bd3ba176d78a10f69724c2358f67d561237ed3
bjeutter <jeutter@zeichenkraftwerk.de>
2015-11-25 21:49:41 +0100

    Removed unused code

--- assistance-platform-service/app/messaging/JmsMessagingServiceFactory.java
+++ assistance-platform-service/app/messaging/JmsMessagingServiceFactory.java
@@ -1,14 +1,9 @@
 package messaging;
 
-import play.Play;
 import de.tudarmstadt.informatik.tk.assistanceplatform.services.messaging.jms.JmsMessagingService;
 
 public class JmsMessagingServiceFactory {
 	public static JmsMessagingService createServiceFromConfig() {
-		String user = Play.application().configuration().getString("activemq.user");
-		String password = Play.application().configuration().getString("activemq.password");
-		String broker = Play.application().configuration().getString("activemq.broker");
-		
 		return new JmsMessagingService(JmsMessagingServiceConfig.getBroker(), JmsMessagingServiceConfig.getUser(), JmsMessagingServiceConfig.getPassword());
 	}
 }
