287863d14561db902ff42851bced706d539658e8
bjeutter <jeutter@zeichenkraftwerk.de>
2015-11-28 16:53:48 +0100

    Changed mobileCarrierCode to mobileCountryCode in Mobile Connection event

--- SharedAssistancePlatformAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/data/sensor/MobileConnection.java
+++ SharedAssistancePlatformAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/data/sensor/MobileConnection.java
@@ -6,110 +6,45 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @Table(name = "sensor_mobileconnection")
 public class MobileConnection extends SensorData {
 	public String carriername;
-	public String mobileCarrierCode;
+	public String mobileCountryCode;
 	public String mobileNetworkCode;
-	
+
 	@JsonProperty(value = "voipAvailable")
 	public boolean voipAvailableOptional;
-	
+
 	public MobileConnection() {
 		super();
 	}
-	
-	
 
 	public String getCarriername() {
 		return carriername;
 	}
 
-
-
 	public void setCarriername(String carriername) {
 		this.carriername = carriername;
 	}
 
-
-
-	public String getMobileCarrierCode() {
-		return mobileCarrierCode;
+	public String getMobileCountryCode() {
+		return mobileCountryCode;
 	}
 
-
-
-	public void setMobileCarrierCode(String mobileCarrierCode) {
-		this.mobileCarrierCode = mobileCarrierCode;
+	public void setMobileCountryCode(String mobileCountryCode) {
+		this.mobileCountryCode = mobileCountryCode;
 	}
-
-
 
 	public String getMobileNetworkCode() {
 		return mobileNetworkCode;
 	}
 
-
-
 	public void setMobileNetworkCode(String mobileNetworkCode) {
 		this.mobileNetworkCode = mobileNetworkCode;
 	}
-
-
 
 	public boolean isVoipAvailableOptional() {
 		return voipAvailableOptional;
 	}
 
-
-
 	public void setVoipAvailableOptional(boolean voipAvailableOptional) {
 		this.voipAvailableOptional = voipAvailableOptional;
-	}
-
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result
-				+ ((carriername == null) ? 0 : carriername.hashCode());
-		result = prime
-				* result
-				+ ((mobileCarrierCode == null) ? 0 : mobileCarrierCode
-						.hashCode());
-		result = prime
-				* result
-				+ ((mobileNetworkCode == null) ? 0 : mobileNetworkCode
-						.hashCode());
-		result = prime * result + (voipAvailableOptional ? 1231 : 1237);
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MobileConnection other = (MobileConnection) obj;
-		if (carriername == null) {
-			if (other.carriername != null)
-				return false;
-		} else if (!carriername.equals(other.carriername))
-			return false;
-		if (mobileCarrierCode == null) {
-			if (other.mobileCarrierCode != null)
-				return false;
-		} else if (!mobileCarrierCode.equals(other.mobileCarrierCode))
-			return false;
-		if (mobileNetworkCode == null) {
-			if (other.mobileNetworkCode != null)
-				return false;
-		} else if (!mobileNetworkCode.equals(other.mobileNetworkCode))
-			return false;
-		if (voipAvailableOptional != other.voipAvailableOptional)
-			return false;
-		return true;
 	}
 }
