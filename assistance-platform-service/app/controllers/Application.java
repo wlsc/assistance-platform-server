62d5cd96283091c1809dd29c304e1f9ed9827f13
bjeutter <jeutter@zeichenkraftwerk.de>
2015-12-03 12:12:12 +0100

    Added local IP endpoint for load balancing testing

--- assistance-platform-service/app/controllers/Application.java
+++ assistance-platform-service/app/controllers/Application.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -8,5 +11,17 @@ public class Application extends Controller {
     public Result index() {
        // return ok(index.render("Your new application is ready."));
     	return ok("Your new application is ready.");
+    }
+    
+    public Result ip() {
+    	InetAddress thisIp = null;
+		try {
+			thisIp = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return ok(thisIp.getHostAddress());
     }
 }
