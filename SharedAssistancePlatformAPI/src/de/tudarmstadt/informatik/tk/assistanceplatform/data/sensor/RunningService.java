e11380f2e22afaa3ad35c4180467a8febce8aa94
bjeutter <jeutter@zeichenkraftwerk.de>
2015-12-07 18:00:26 +0100

    Added running service event

---
+++ SharedAssistancePlatformAPI/src/de/tudarmstadt/informatik/tk/assistanceplatform/data/sensor/RunningService.java
@@ -0,0 +1,31 @@
+package de.tudarmstadt.informatik.tk.assistanceplatform.data.sensor;
+
+import com.datastax.driver.mapping.annotations.Table;
+
+@Table(name = "sensor_runningservice")
+public class RunningService extends SensorData {
+	public String packageName;
+	public String className;
+	
+	public RunningService(String packageName, String className) {
+		super();
+		this.packageName = packageName;
+		this.className = className;
+	}
+
+	public String getPackageName() {
+		return packageName;
+	}
+
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+}
