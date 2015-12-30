be0a0662cea630f8ade74ab8dd16b176b915ff2f
bjeutter <jeutter@zeichenkraftwerk.de>
2015-12-30 23:07:17 +0100

    Detecting if tests are running and disabling periodic jobs then

--- assistance-platform-service/app/Global.java
+++ assistance-platform-service/app/Global.java
@@ -21,24 +21,25 @@ import errors.APIError;
 public class Global extends GlobalSettings {
 	@Override
 	public void onStart(Application app) {
-		schedulePeriodicModuleCheck();
-		
-		while(proxySessionCheck() == null);
+		if (!play.Play.isTest()) {
+			schedulePeriodicModuleCheck();
+			while (proxySessionCheck() == null);
+		}
 	}
-	
+
 	private CassandraSessionProxy proxySessionCheck() {
 		CassandraSessionProxy proxy = null;
 
 		try {
 			proxy = CassandraSessionProxyFactory.getSessionProxy();
-		} catch(NoHostAvailableException e) {
+		} catch (NoHostAvailableException e) {
 			Logger.warn("Cassandra not up, waiting 2 seconds... ");
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
-		} catch(AuthenticationException e) {
+		} catch (AuthenticationException e) {
 			Logger.warn("Waiting 2secs for Cassandra to setup auth... ");
 			try {
 				Thread.sleep(2000);
@@ -46,45 +47,51 @@ public class Global extends GlobalSettings {
 				e1.printStackTrace();
 			}
 		}
-		
+
 		return proxy;
 	}
 
 	private void schedulePeriodicModuleCheck() {
-		    ModuleAliveChecker aliveChecker = new ModuleAliveChecker();
-		    
-		    Akka.system().scheduler().schedule(Duration.create(0, TimeUnit.MILLISECONDS), //Initial delay 0 milliseconds
-		    	    Duration.create(30, TimeUnit.MINUTES),     //Frequency 30 minutes
-		    	    new Runnable() {
-	                    @Override
-	                    public void run() {
-	                        aliveChecker.checkModulesAliveStati();
-	                    }
-	                },
-	                Akka.system().dispatcher()
-		    	);
-	  }
+		ModuleAliveChecker aliveChecker = new ModuleAliveChecker();
+
+		Akka.system().scheduler()
+				.schedule(Duration.create(0, TimeUnit.MILLISECONDS), // Initial
+																		// delay
+																		// 0
+																		// milliseconds
+						Duration.create(30, TimeUnit.MINUTES), // Frequency 30
+																// minutes
+						new Runnable() {
+							@Override
+							public void run() {
+								aliveChecker.checkModulesAliveStati();
+							}
+						}, Akka.system().dispatcher());
+	}
 
 	@Override
-    public Promise<play.mvc.Result> onError(RequestHeader request, Throwable t) {
+	public Promise<play.mvc.Result> onError(RequestHeader request, Throwable t) {
 		return F.Promise.promise(() -> {
-			return play.mvc.Results.internalServerError(
-					RestController.errorInJson( new APIError(0, t.getMessage())));
+			return play.mvc.Results.internalServerError(RestController
+					.errorInJson(new APIError(0, t.getMessage())));
 		});
-    }
-    
-    @Override
-    public Promise<play.mvc.Result> onHandlerNotFound(RequestHeader request) {
-    	return F.Promise.promise(() -> {
-    		return play.mvc.Results.badRequest(
-        		RestController.errorInJson(new APIError(0, "Handler not found. Invalid route.")));
-    	});
-    }
+	}
 
-    @Override
-    public Promise<play.mvc.Result> onBadRequest(RequestHeader request, String error) {
-    	return F.Promise.promise(() -> {
-    		return play.mvc.Results.badRequest(RestController.errorInJson(new APIError(0, error)));
-    	});
-    }
+	@Override
+	public Promise<play.mvc.Result> onHandlerNotFound(RequestHeader request) {
+		return F.Promise.promise(() -> {
+			return play.mvc.Results.badRequest(RestController
+					.errorInJson(new APIError(0,
+							"Handler not found. Invalid route.")));
+		});
+	}
+
+	@Override
+	public Promise<play.mvc.Result> onBadRequest(RequestHeader request,
+			String error) {
+		return F.Promise.promise(() -> {
+			return play.mvc.Results.badRequest(RestController
+					.errorInJson(new APIError(0, error)));
+		});
+	}
 }
