d55e8f5d2bc6801bb12909f67092cd4c7894b78b
bjeutter <jeutter@zeichenkraftwerk.de>
2016-01-23 12:54:40 +0100

    Missing DTO for procesing time logging

---
+++ assistance-platform-service/app/dto/ResponsetimeLog.java
@@ -0,0 +1,25 @@
+package dto;
+
+import java.util.List;
+
+public class ResponsetimeLog {
+	public ResponsetimeLog() {}
+	
+	public static class ResponseTimeEntry {
+		public ResponseTimeEntry() {
+		}
+
+		public long startTime;
+		public long responseTime;
+		public long processingTime;
+		public long bodySize;
+		public int eventsNumber;
+		public String networkType;
+	}
+	
+	public long userId;
+	
+	public long deviceId;
+	
+	public List<ResponseTimeEntry> data;
+}
