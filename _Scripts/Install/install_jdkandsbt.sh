77325899ada00b993087860a7af8c19afc106dfb
Wladimir Schmidt <wlsc.dev@gmail.com>
2016-08-23 12:29:02 +0200

    - removed sudo from each line in bash install script of jdk and sbt tools

--- _Scripts/Install/install_jdkandsbt.sh
+++ _Scripts/Install/install_jdkandsbt.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # JDK
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
+apt-get install python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
 
 # SBT
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-sudo apt-get update
-sudo apt-get install sbt
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+apt-get update
+apt-get install sbt
