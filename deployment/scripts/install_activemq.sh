8a2b02540bf08d00dcbf9cf7efd5bb682ae4c9ff
Wladimir Schmidt <wlsc.dev@gmail.com>
2016-09-06 10:49:54 +0200

    * updated activemq version in the installation script

--- deployment/scripts/install_activemq.sh
+++ deployment/scripts/install_activemq.sh
@@ -5,15 +5,15 @@ useradd -m activemq -d /opt/activemq
 
 # Download and extract activemq
 cd /opt/activemq
-wget http://ftp-stud.hs-esslingen.de/pub/Mirrors/ftp.apache.org/dist/activemq/5.12.0/apache-activemq-5.12.0-bin.tar.gz
-tar zxvf apache-activemq-5.12.0-bin.tar.gz
-rm apache-activemq-5.12.0-bin.tar.gz
+wget http://www.apache.org/dyn/closer.cgi?filename=/activemq/5.14.0/apache-activemq-5.14.0-bin.tar.gz&action=download
+tar zxvf apache-activemq-5.14.0-bin.tar.gz
+rm apache-activemq-5.14.0-bin.tar.gz
 
 # Symlink for current version
-ln -snf apache-activemq-5.12.0 current
+ln -snf apache-activemq-5.14.0 current
 
 # Set rights for the activemq folder
-chown -R activemq:users apache-activemq-5.12.0
+chown -R activemq:users apache-activemq-5.14.0
 
 # Prepare settings for activemq
 cp current/bin/env /etc/default/activemq
